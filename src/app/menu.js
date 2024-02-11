b2175872499c970c5d1b003b221579984363d256
pierregarcia1 <pierre.ggarcia01@gmail.com>
2024-02-11 00:27:34 -0600

    Custom menu base.

---
+++ src/app/menu.js
@@ -0,0 +1,20 @@
+function hideCustomContextMenu() {
+    document.getElementById('customContextMenu').style.display = "none";
+}
+ 
+// toggling the menu on right click to the page
+function showCustomContextMenu(event) {
+    event.preventDefault();
+    var myContextMenu = document.getElementById('customContextMenu');
+    if (myContextMenu.style.display == "block") {
+       myContextMenu.style.display = "none";
+    }
+    else {
+       myContextMenu.style.display = "block";
+       myContextMenu.style.left = event.pageX + "px";
+       myContextMenu.style.top = event.pageY + "px";
+    }
+}
+
+document.onclick = hideCustomContextMenu;
+document.oncontextmenu = showCustomContextMenu;

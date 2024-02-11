366a811e49fb892fab9116e709cb72c36316480d
Brandon Robertiello <77466123+Br4nd0nR@users.noreply.github.com>
2024-02-11 05:02:55 -0600

    Start of Messaging

--- src/app/speech.js
+++ src/app/speech.js
@@ -1,9 +1,39 @@
+function promptInput() {
+
+    var inp = document.getElementById("userSpeechBubble");
+
+    inp.style.display = "block";
+
+}
+
+function askAlfredo(msg) {
+
+    say(msg);
+
+}
+
 function say(msg){
 
     var sb = document.getElementById("alfredoSpeechBubble")
     
     sb.innerHTML = msg;
 
+    showMessage();
+
+}
+
+function showMessage() {
+
+    var sb = document.getElementById("alfredoSpeechBubble")
+
     sb.style.display = "block";
+
+}
+
+function hideMessage() {
+
+    var sb = document.getElementById("alfredoSpeechBubble")
+
+    sb.style.display = "none";
 
 }

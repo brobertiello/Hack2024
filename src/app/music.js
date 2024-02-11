ae2f2b0cff4962533fb67372bfb4f984de93479c
Brandon Robertiello <77466123+Br4nd0nR@users.noreply.github.com>
2024-02-11 05:21:39 -0600

    Improved Dance Music

--- src/app/music.js
+++ src/app/music.js
@@ -2,10 +2,12 @@ var isPlaying = false;
 
 function startMusic() {
     var audio = document.getElementById("danceAudio");
+    audio.currentTime = 0;
     audio.play();
 }
 
 function stopMusic() {
     var audio = document.getElementById("danceAudio");
     audio.pause();
+    audio.currentTime = 0;
 }
